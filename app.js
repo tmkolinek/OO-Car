68875adc22d92e52454c28703fa7cfdddf9c9ffc
Thomas Kolinek <kolinektommy@gmail.com>
2023-03-15 14:19:25 -0700

    finished assignment

---
+++ app.js
@@ -0,0 +1,52 @@
+class Vehicle{
+    constructor(make, model, year){
+        this.make = make;
+        this.model =  model;
+        this.year = year;
+    }
+
+    honk(){
+        return 'Beep.';
+    }
+
+    toString(){
+        return `This vehicle is a ${this.make} ${this.model} from ${this.year}`;
+    }
+}
+
+class Car extends Vehicle{
+    constructor(make,model, year){
+        super(make,model,year);
+        this.numWheels = 4;
+    }
+}
+
+class Motorcycle extends Vehicle{
+    constructor(make, model, year){
+        super(make,model,year);
+        this.numWheels = 2;
+    }
+
+    revEngine(){
+        return 'VROOM!!!';
+    }
+}
+
+class Garage{
+    constructor(capacity){
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+
+    add(vehicle){
+        if(!(vehicle instanceof Vehicle)){
+            return "Only vehicles are allowed in here!";
+        }
+        if (this.vehicles.length >= capacity){
+            return "Sorry we're full";
+        }
+        
+        this.vehicles.push(vehicle);
+        return "Vehicle added!";
+    }
+}
